ce5a0bd561cf55634a2601f19001785e41550a04
kinsleykajiva <kajivakinsley@gmail.com>
2024-01-26 16:09:16 +0200

    updates

--- src/main/java/africa/jopen/sdk/models/events/JanusWebRTCStateEvent.java
+++ src/main/java/africa/jopen/sdk/models/events/JanusWebRTCStateEvent.java
@@ -16,31 +16,22 @@ public class JanusWebRTCStateEvent {
 	 * The record representing a WebRTC state event within Janus, including ICE (Interactive
 	 * Connectivity Establishment) details, stream ID, and component ID.
 	 */
-	public record Event(String ice, int stream_id, int component_id) {
+	public record Event(String ice, int stream_id, int component_id,String local_candidate,String remote_candidate) {
 	}
 	
-	/*CREATE TABLE IF NOT EXISTS janus_ice (
-				    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-				    session BIGINT(30) NOT NULL,
-				    handle BIGINT(30) NOT NULL,
-				    stream INT NOT NULL,
-				    component INT NOT NULL,
-				    state VARCHAR(30) NOT NULL,
-				    timestamp DATETIME NOT NULL,
-				    INDEX janus_idx_session_handle (session, handle),
-				    INDEX janus_idx_stream_component (stream, component),
-				    INDEX janus_idx_state (state),
-				    INDEX janus_idx_timestamp (timestamp)
-				);*/
+
 	
 	public String trackInsert( JanusWebRTCStateEvent.Root root ) {
 		
-		return "INSERT INTO janus_ice (session, handle, stream, component, state, timestamp) VALUES ("
+		return "INSERT INTO janus_ice (session, handle, stream, component, state, timestamp,local_candidate,remote_candidate) VALUES ("
 				+ root.session_id() + ", "
 				+ root.handle_id() + ", "
 				+ root.event().stream_id() + ", "
 				+ root.event().component_id() + ", '"
 				+ root.event().ice() + "', '"
-				+ new Timestamp(root.timestamp()) + "')";
+				+ new Timestamp(root.timestamp()) + "' , '" +
+				root.event().local_candidate() + "','" +
+				root.event().remote_candidate() + "'); ";
+				
 	}
 }
