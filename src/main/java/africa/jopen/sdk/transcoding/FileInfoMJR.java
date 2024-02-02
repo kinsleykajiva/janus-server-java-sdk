c83fe4365b4abd3188c295e2a74316bf2b6a2641
kinsleykajiva <kajivakinsley@gmail.com>
2024-02-02 09:23:56 +0200

    Add FileInfoMJR record and update MediaFactory
    A new record named FileInfoMJR is added and significant changes are made in the MediaFactory class. This includes importing additional libraries, adding a List of FileInfoMJR type, and implementing methods to handle new behaviors such as reading video/audio files and fetching file information.

---
+++ src/main/java/africa/jopen/sdk/transcoding/FileInfoMJR.java
@@ -0,0 +1,4 @@
+package africa.jopen.sdk.transcoding;
+
+public record FileInfoMJR (String videoRoom, String userId, long startTime, String fileTypeVideoAudio, String id, java.io.File file) {
+}
--- src/main/java/africa/jopen/sdk/transcoding/MediaFactory.java
+++ src/main/java/africa/jopen/sdk/transcoding/MediaFactory.java
@@ -5,6 +5,18 @@ import africa.jopen.sdk.SdkUtils;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @ApiStatus.NonExtendable
 public class MediaFactory {
 	
@@ -12,6 +24,7 @@ public class MediaFactory {
 	private String            roomId;
 	private String            recordingFolder;
 	private String            outputFolder;
+	private List<FileInfoMJR> fileInfoMJRs = new ArrayList<>();
 	
 	
 	private MediaFactory() {
@@ -34,5 +47,62 @@ public class MediaFactory {
 		if (!SdkUtils.folderExists(outputFolder)) {
 			throw new RuntimeException("Output folder does not exist");
 		}
+		if (mediaOutputTarget == MediaOutputTarget.VIDEO_ROOM_PLUGIN) {
+			// look for files start with 'videoroom' ends with mjr in folder recordingFolder
+			List<String> matchingFiles = new ArrayList<>();
+			Path         dir           = Paths.get(recordingFolder);
+			try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+				for (Path path : stream) {
+					String fileName = path.getFileName().toString();
+					if (fileName.startsWith("videoroom-" + roomId) && fileName.endsWith(".mjr")) {
+						matchingFiles.add(fileName);
+					}
+				}
+				matchingFiles.forEach(file -> {
+					var  fullFilePath = file;
+					Path filePath     = Paths.get(file);
+					
+					if (!Files.exists(filePath)) {
+						// is not full path,its just name
+						fullFilePath = recordingFolder + File.separator + file;
+					}
+					fileInfoMJRs.add(getFileMJRInfo(fullFilePath));
+				});
+				
+				fileInfoMJRs.removeIf(Objects::isNull);// stream to remove null values in fileInfoMJRs
+				
+				if (fileInfoMJRs.size() % 2 != 0) {
+					throw new RuntimeException("The files do not match . For each video mjr file there is a audio mjr file and vice versa");
+				}
+			} catch (IOException e) {
+				throw new RuntimeException("Error reading directory", e);
+			}
+			
+			
+		}
+		
+	}
+	
+	public FileInfoMJR getFileMJRInfo( String filePath ) {
+		if (filePath == null || filePath.isEmpty()) {
+			return null;
+		}
+		
+		if (mediaOutputTarget == MediaOutputTarget.VIDEO_ROOM_PLUGIN) {
+			Pattern pattern = Pattern.compile("videoroom-(\\w+)-user-(\\d+)-(\\d+)-(audio|video)-(\\d+)\\.mjr");
+			Matcher matcher = pattern.matcher(filePath);
+			
+			if (matcher.find()) {
+				String videoRoom = matcher.group(1);
+				String userId    = matcher.group(2);
+				String startTime = matcher.group(3);
+				String fileType  = matcher.group(4);
+				String id        = matcher.group(5);
+				
+				return new FileInfoMJR(videoRoom, userId, Long.parseLong(startTime), fileType, id, new File(filePath));
+			}
+		}
+		
+		return null;
 	}
 }
