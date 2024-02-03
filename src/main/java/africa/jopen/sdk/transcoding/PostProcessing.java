5500b0bec5c4a8fbc1e1aa9b81e09fb78e43d04c
kinsleykajiva <kajivakinsley@gmail.com>
2024-02-03 11:28:14 +0200

    Implement media factory functionality in Playground
    Added media factory instantiation in the Playground class's main function, along with post-processing start, end and failure handlers. This helps handle the life cycle of processing files. Also updated Javadoc documentation for relevant classes.

--- src/main/java/africa/jopen/sdk/transcoding/PostProcessing.java
+++ src/main/java/africa/jopen/sdk/transcoding/PostProcessing.java
@@ -3,6 +3,9 @@ package africa.jopen.sdk.transcoding;
 import java.io.File;
 import java.util.List;
 
+/**
+ * The PostProcessing interface defines the callback methods for handling the processing lifecycle of files.
+ */
 public interface PostProcessing {
 	
 	void onProcessingStarted( String roomId, long millisecondsTimeStamp, List<FileInfoMJR> fileInfoMJRs,Thread thread);
