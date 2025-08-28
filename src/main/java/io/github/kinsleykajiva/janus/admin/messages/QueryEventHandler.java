4d6fab9168fc859dd8925bd0b6fda58a06971574
Kinsley <kajivakinsley@gmail.com>
2025-08-28 19:52:31 +0200

    refactor(QueryEventHandler): convert to recordConvert QueryEventHandler class to a record for immutability andconciseness. Add eventhandler to project dictionary.

--- src/main/java/io/github/kinsleykajiva/janus/admin/messages/QueryEventHandler.java
+++ src/main/java/io/github/kinsleykajiva/janus/admin/messages/QueryEventHandler.java
@@ -2,23 +2,14 @@ package io.github.kinsleykajiva.janus.admin.messages;
 
 import org.json.JSONObject;
 
-public class QueryEventHandler {
-    private final String transaction;
-    private final String handler;
-    private final JSONObject request;
-
-    public QueryEventHandler(String transaction, String handler, JSONObject request) {
-        this.transaction = transaction;
-        this.handler = handler;
-        this.request = request;
-    }
-
-    public JSONObject toJson() {
-        JSONObject json = new JSONObject();
-        json.put("janus", "query_eventhandler");
-        json.put("transaction", transaction);
-        json.put("handler", handler);
-        json.put("request", request);
-        return json;
-    }
+public record QueryEventHandler(String transaction, String handler, JSONObject request) {
+	
+	public JSONObject toJson() {
+		JSONObject json = new JSONObject();
+		json.put("janus", "query_eventhandler");
+		json.put("transaction", transaction);
+		json.put("handler", handler);
+		json.put("request", request);
+		return json;
+	}
 }
