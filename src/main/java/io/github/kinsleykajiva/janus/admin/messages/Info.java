ee432edd21ad1d840386c81fd9edca6746fccc66
Kinsley <kajivakinsley@gmail.com>
2025-08-28 20:21:07 +0200

    refactor(admin): convert Info class to a record- Change Info class to a record for immutability and less boilerplate- Keep the toJson() method implementation

--- src/main/java/io/github/kinsleykajiva/janus/admin/messages/Info.java
+++ src/main/java/io/github/kinsleykajiva/janus/admin/messages/Info.java
@@ -2,17 +2,12 @@ package io.github.kinsleykajiva.janus.admin.messages;
 
 import org.json.JSONObject;
 
-public class Info {
-    private final String transaction;
-
-    public Info(String transaction) {
-        this.transaction = transaction;
-    }
-
-    public JSONObject toJson() {
-        JSONObject json = new JSONObject();
-        json.put("janus", "info");
-        json.put("transaction", transaction);
-        return json;
-    }
+public record Info(String transaction) {
+	
+	public JSONObject toJson() {
+		JSONObject json = new JSONObject();
+		json.put("janus", "info");
+		json.put("transaction", transaction);
+		return json;
+	}
 }
